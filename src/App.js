ef8ac7ad10b818e2444f2f07e7ae581e81298401
Ajay Marathe <ajay@learny.co>
2021-04-16 08:55:05 +0530

    added simple counter app based on redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Hello world</h2>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  add(){
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  remove(){
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Counter App</h2>
+        <p>The count is: {this.state.count}</p>
+
+        <button onClick={()=> this.add()}>Add</button>
+        <button onClick={()=> this.remove()}>Remove</button>
+      </div>
+    );
+  }
 }
 
 export default App;
